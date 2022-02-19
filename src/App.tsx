316932c727a7a4b085c35cd9a73b1e0ab81d84d1
TaikiCode <78462662+TaikiCode@users.noreply.github.com>
2022-02-19 13:25:29 +0900

    reducerの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { VFC } from "react";
+import { useReducer, VFC } from "react";
 import "./App.css";
+import { GAME_MESSAGE } from "./contents/gameStatus";
+import { initialState } from "./reducer/game/initialState";
+import { reducer } from "./reducer/game/reducer";
 
 const App: VFC = () => {
+
+  const [gameState, gameDispatch] = useReducer(reducer, initialState)
+
+  console.log(gameState)
+
   return (
     <div className="blackJackTable">
+      {typeof GAME_MESSAGE["win"]}
 
     </div>
   );
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,35 @@
+import { GAME_MESSAGE } from './../contents/gameStatus';
+
 export interface Card {
   code: string;
   image: string;
   suit: string;
   value: string;
   cardValue: string;
+}
+
+export interface PlayerInfo {
+    total: number
+    totalAlt: number
+    cards: Card[]
+    betSize: number
+    budget: number
+}
+
+export interface DealerInfo {
+    total: number
+    totalAlt: number
+    cards: Card[]
+}
+
+export interface Status {
+    isPlaying: boolean
+    resultMsg: string
+}
+
+export interface Game {
+    deck: Card[]
+    playerInfo:　PlayerInfo
+    dealerInfo: DealerInfo
+    status: Status
 }
