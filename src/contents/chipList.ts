389ec5ad454d5d2fbde2cf62450cea71b06014f0
TaikiCode <78462662+TaikiCode@users.noreply.github.com>
2022-02-19 09:27:43 +0900

    データのセット

---
+++ src/contents/chipList.ts
@@ -0,0 +1,22 @@
+export const CHIP_LIST = [
+  {
+    value: 1,
+    color: "white",
+  },
+  {
+    value: 5,
+    color: "red",
+  },
+  {
+    value: 10,
+    color: "blue",
+  },
+  {
+    value: 50,
+    color: "green",
+  },
+  {
+    value: 100,
+    color: "black",
+  },
+];
